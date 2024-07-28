9c79ca51230b21c2680a5e481c0e152ecf07e1e9
Teagan glenn <that@teagantotally.rocks>
2024-07-28 17:39:46 +0000

    Log

--- custom_components/auto_areas/auto_entity.py
+++ custom_components/auto_areas/auto_entity.py
@@ -133,6 +133,7 @@ class AutoEntity(Entity, Generic[_TEntity, _TDeviceClass]):
         """Track entity state changes."""
         entity_ids = self.get_sensor_entities()
         if sorted(entity_ids) == sorted(self.entity_ids):
+            LOGGER.info("%s - No change in entities, skipping", self.entity_id)
             return
         if self.unsubscribe:
             try:
@@ -140,6 +141,7 @@ class AutoEntity(Entity, Generic[_TEntity, _TDeviceClass]):
             except ValueError:
                 pass
         self.unsubscribe = None
+        LOGGER.info("%s - Found %s", self.entity_id, ",".join(self.entity_ids))
         self.entity_ids = entity_ids
         for entity_id in [entity_id for entity_id in self.entity_states.keys() if entity_id not in entity_ids]:
             self.entity_states.pop(entity_id, None)
